ca742c7fcd7db90b7797c2f6b102a030f21c8e01
zlatohlavkova <102213005+zlatohlavkova@users.noreply.github.com>
2022-05-01 23:24:32 +0000

    Deploying to docs from @ books-are-next/topol-kloktat-dehet@accd959927e0e74c50ddad49d6af0616601ddea2 🚀

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 
 /* global self, caches, fetch */
 
-const CACHE = 'cache-1cb3b60';
+const CACHE = 'cache-accd959';
 
 self.addEventListener('install', e => {
   e.waitUntil(precache()).then(() => self.skipWaiting());
