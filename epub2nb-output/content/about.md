ed5f3c62e372752faef65159902cd93ea7833efa
adelaweidischova <48671822+adelaweidischova@users.noreply.github.com>
2022-08-17 10:35:16 +0000

    Deploying to docs from @ books-are-next/topol-kloktat-dehet@9acfcb16690a571c8dc5f43b5f50d2fe9fe9c29c 🚀

--- epub2nb-output/content/about.md
+++ epub2nb-output/content/about.md
@@ -7,8 +7,7 @@ Kloktat dehet**
 Redakce Jaroslava Bednářová  
 Vydala Městská knihovna v Praze  
 Mariánské nám. 1, 115 72 Praha 1  
-ISBN 80-7215-255-6 (webová kniha)  
-V MKP 2. elektronické vydání z 21. 7. 2022.
+V MKP 2. elektronické vydání z 17. 8. 2022.
 
 ***
 
--- service-worker.js
+++ service-worker.js
@@ -764,7 +764,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 /// <reference lib="WebWorker" />
 require("regenerator-runtime");
 const sw = self;
-const CACHE = 'cache-7dd827a';
+const CACHE = 'cache-9acfcb1';
 /**
  * IndexedDB singleton wrapper used to store persistent information with an predefined {@link Schema}
  */
